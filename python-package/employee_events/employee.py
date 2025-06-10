9374b01d274d0345bab60da985a3bd3e0b88b428
karmadolma.grg@gmail.com <karmadolma.grg@gmail.com>
2025-06-10 15:31:40 +0700

    updated employee.py file

--- python-package/employee_events/employee.py
+++ python-package/employee_events/employee.py
@@ -1,24 +1,27 @@
 # Import the QueryBase class
-#### YOUR CODE HERE
+from QueryBase import QueryBase
 
 # Import dependencies needed for sql execution
 # from the `sql_execution` module
-#### YOUR CODE HERE
+from sqlite3 import connect
+from pathlib import Path
+from functools import wraps
+import pandas as pd
 
 # Define a subclass of QueryBase
 # called Employee
-#### YOUR CODE HERE
+class Employee(QueryBase):
 
     # Set the class attribute `name`
     # to the string "employee"
-    #### YOUR CODE HERE
+   name = "employee"
 
 
     # Define a method called `names`
     # that receives no arguments
     # This method should return a list of tuples
     # from an sql execution
-    #### YOUR CODE HERE
+    def names():
         
         # Query 3
         # Write an SQL query
@@ -27,14 +30,20 @@
         # 2. The employee's id
         # This query should return the data
         # for all employees in the database
-        #### YOUR CODE HERE
-    
+        query = """
+        		SELECT first_name || " " || last_name AS full_name,
+        			employee_id 
+        		FROM employee
+        
+        """
+        return self.tuple_query(query)
+
 
     # Define a method called `username`
     # that receives an `id` argument
     # This method should return a list of tuples
     # from an sql execution
-    #### YOUR CODE HERE
+    def username(self, id):
         
         # Query 4
         # Write an SQL query
@@ -42,7 +51,13 @@
         # Use f-string formatting and a WHERE filter
         # to only return the full name of the employee
         # with an id equal to the id argument
-        #### YOUR CODE HERE
+        query = f"""
+        		SELECT first_name || " " || last_name AS full_name
+        		FROM employee
+        		WHERE employee_id = {id}
+        
+        """
+        return self.tuple_query(query)
 
 
     # Below is method with an SQL query
@@ -54,8 +69,8 @@
     # the sql query
     #### YOUR CODE HERE
     def model_data(self, id):
-
-        return f"""
+    
+    	query = f"""
                     SELECT SUM(positive_events) positive_events
                          , SUM(negative_events) negative_events
                     FROM {self.name}
@@ -63,3 +78,5 @@
                         USING({self.name}_id)
                     WHERE {self.name}.{self.name}_id = {id}
                 """
+        
+        return self.pandas_query(query)
